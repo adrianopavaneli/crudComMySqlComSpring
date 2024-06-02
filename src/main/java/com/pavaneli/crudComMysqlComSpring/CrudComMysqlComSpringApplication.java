b4e449fbd3c48a6493bd527776802fcf3612945c
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-06-02 17:46:30 -0300

    first commit

---
+++ src/main/java/com/pavaneli/crudComMysqlComSpring/CrudComMysqlComSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pavaneli.crudComMysqlComSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudComMysqlComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudComMysqlComSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavaneli/crudComMysqlComSpring/CrudComMysqlComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavaneli.crudComMysqlComSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudComMysqlComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
